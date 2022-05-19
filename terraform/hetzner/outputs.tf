32c1c627c831fb1b3133a59a17f7cce82248ec02
jay-dee7 <jasdeepsingh.uppal@gmail.com>
2022-05-19 12:06:45 +0530

    Add: description for all the output blocks
    Signed-off-by: jay-dee7 <jasdeepsingh.uppal@gmail.com>

--- terraform/hetzner/outputs.tf
+++ terraform/hetzner/outputs.tf
@@ -1,45 +1,56 @@
 output "seed_servers" {
-  value = hcloud_server.seed
+  value       = hcloud_server.seed
+  description = "Set of seed nodes running on Hetzner Cloud"
 }
 
 output "seed_volumes" {
-  value = hcloud_volume.seed
+  value       = hcloud_volume.seed
+  description = "Set of volumes used by seed nodes"
 }
 
 output "seeds_firewall" {
-  value = hcloud_firewall.seed
+  value       = hcloud_firewall.seed
+  description = "This firewall is used to restrict inbound/outbound traffic for seed nodes"
 }
 
 output "sentry_servers" {
-  value = hcloud_server.sentry
+  value       = hcloud_server.sentry
+  description = "Set of sentry nodes running on Hetzner Cloud"
 }
 
 output "sentry_volumes" {
-  value = hcloud_volume.sentry
+  value       = hcloud_volume.sentry
+  description = "Set of volumes used by sentry nodes"
 }
 
 output "sentries_firewall" {
-  value = hcloud_firewall.sentry
+  value       = hcloud_firewall.sentry
+  description = "This firewall is used to restrict inbound/outbound traffic for sentry nodes"
 }
 
 output "vpc" {
-  value = hcloud_network.cheqd_network
+  value       = hcloud_network.cheqd_network
+  description = "Private Network for the entire fleet of services running on Hetzner Cloud"
 }
 
 output "validator_servers" {
-  value = hcloud_server.validator
+  value       = hcloud_server.validator
+  description = "Set of Validator nodes running on Hetzner Cloud"
 }
 
 output "seed_floating_ip" {
-  value = hcloud_floating_ip.seed
+  value       = hcloud_floating_ip.seed
+  description = "Set of Hetzner Floating IPs used by seed nodes"
 }
 
 output "sentry_floating_ip" {
-  value = hcloud_floating_ip.sentry
+  value       = hcloud_floating_ip.sentry
+  description = "Set of Hetzner Floating IPs used by sentry nodes"
 }
 
 output "validator_floating_ip" {
-  value = hcloud_floating_ip.validator
+  value       = hcloud_floating_ip.validator
+  description = "Set of Hetzner Floating IPs used by validator nodes"
 }
 
 output "server_ips" {
@@ -47,6 +58,7 @@ output "server_ips" {
     ipv4_address = v.ip_address
     }
   }
+  description = "This is a Set of all server IPs (Seeds, Sentries, and Validators) with key as node name (like seed1-ap-mainnet) and their value the IPv4 Address of the node. This is useful for doing easy DNS mapping using a for_each"
 }
 
 locals {
